7b10f5f8d7223dbc14380bb65c548d92957afcd1
Patrick42702 <patrick42702@yahoo.com>
2024-02-03 18:19:25 -0500

    making styling changes

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export function Layout() {
   return (
-    <div className="text-teal-100">
+    <div className="text-teal-100 bg-teal-900">
       <Outlet />
     </div>
       
