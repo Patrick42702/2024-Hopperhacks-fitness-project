81b7d5c35d895e7ae2cf0bbc05389f22b8580460
jamie <jamie@barkume.com>
2024-02-03 12:51:57 -0500

    added models

---
+++ server/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema= new mongoose.Schema({
+    username:{type: String, required: true},
+    password: {type: String, required: true},
+    height: {type: Number},
+    weight: {type: Number},
+    swimWorkouts: {type: [mongoose.Schema.Types.ObjectID], ref: "SwimWorkout", default: []},
+    RunWorkouts: {type: [mongoose.Schema.Types.ObjectID], ref: "RunWorkout", default: []},
+    liftWorkouts: {type: [mongoose.Schema.Types.ObjectID], ref: "LiftWorkout", default: []}
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
