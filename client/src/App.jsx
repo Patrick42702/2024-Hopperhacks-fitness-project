4d879bf9318cb43fb9d410888e98ddf89c69f122
Patrick42702 <patrick42702@yahoo.com>
2024-02-03 14:48:06 -0500

    bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/login";
+import { Login } from "./components/login";
 import { Layout } from "./components/layout";
 
 function App() {
   return (
-    <>
-    <div>
-      Hello
+    <div className="app">
+      <div>Hello</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index path="/"  />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index path="/" element={<Welcome />} />
-          <Route path="login" element={<Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </>
   );
 }
 
