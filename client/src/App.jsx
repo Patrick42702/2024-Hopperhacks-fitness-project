d3cfd24d43027a6f4e0b699af8e8beff0c18e42a
Patrick42702 <patrick42702@yahoo.com>
2024-02-03 14:26:15 -0500

    began login format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import { Layout } from "./components/layout";
 
 function App() {
-
   return (
     <>
-    
+    <div>
+      Hello
+    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index path="/" element={<Welcome />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
