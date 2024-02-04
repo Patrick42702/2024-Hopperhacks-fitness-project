c24b30f041a7e109269c6b6b592ef90f46378fe1
Justin <justinjj0413@gmail.com>
2024-02-04 01:27:15 -0500

    Finished Looks

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -5,62 +5,61 @@ export function Register(){
     <>
       <form target='_self' onSubmit={(e) => submitRegister(e)} action="submit">
         <div className="title">Registration</div>
-        <div className="registration-box bg-blue-500">
+        <div className="registration-box bg-white">
           <div className="fields">
             <div id="email">
               <label id="email-header">Email:</label>
               <br />
-              <input type="email" id="email" name="email" />
+              <input type="email" className="input-field" name="email" />
               <br />
             </div>
 
             <div id="username">
               <label id="username-header">Username:</label>
               <br />
-              <input type="text" className="username" name="username" />
+              <input type="text" className="input-field" name="username" />
               <br />
             </div>
 
             <div id="password">
               <label id="password-header">Password:</label>
               <br />
-              <input type="password" id="password" name="password" />
+              <input type="password" className="input-field" name="password" />
               <br />
             </div>
 
             <div id="cpassword">
               <label id="cpassword-header">Confirm Password:</label>
               <br />
-              <input type="password" id="cpassword" name="cpassword" />
+              <input type="password" className="input-field" name="cpassword" />
               <br />
             </div>
 
             <div id="dob">
               <label htmlFor="dob-header">Date of Birth:</label>
               <br />
-              <input type="date" className="dob-header" name="dob" />
+              <input type="date" className="input-field" name="dob" />
               <br />
             </div>
 
             <div id="height">
               <label htmlFor="height-header">Height:</label>
               <br />
-              <input type="number" id="height-header" name="height" />
+              <input type="number" className="input-field" name="height" />
               <br />
             </div>
 
             <div id="weight">
               <label htmlFor="weight-header">Weight:</label>
               <br />
-              <input type="number" id="weight-header" name="weight" />
-              <br />
+              <input type="number" className="input-field" name="weight" />
             </div>
-
+            
+            <br />
+            <input type="button" className="mx-auto block bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded" value="Register"/>
           </div>
         </div>
         <br />
-        <input type="button" className="mx-auto block bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded" value="Register"/>
-        
       </form>
     </>
   );
