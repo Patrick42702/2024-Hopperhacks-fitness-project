62808fb6954e32457020f07e716961c71e7948e4
Justin <justinjj0413@gmail.com>
2024-02-04 11:06:10 -0500

    Finished Login

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -7,23 +7,31 @@ export function Login() {
 
   return (
     <>
-    <h1>Welcome to ML Fitness!</h1>
-      <form action="submit" id="login-form">
-        <div id="email">
-          <label id="email-header">Email</label>
+    <div className="flex justify-center items-center">
+      <div className="registration-box bg-white">
+        <div className="text-center">
+          <h1>Welcome to MLFitness!</h1>
           <br />
-          <input type="text" id="email-input" ></input>
+          <form action="submit" id="login-form" className="flex flex-col items-center">
+            <div id="email">
+              <label id="email-header">Email</label>
+              <br />
+              <input type="email" className="input-field" ></input>
+              <br />
+            </div>
+            <div className="password">
+              <label id="password-header">Password</label>
+              <br />
+              <input type="password" className="input-field"/>  
+            </div>
+            <div>
+              <br />
+              <a href="" onClick={handleClick}>Sign up if you haven't already</a>
+            </div>
+        </form>
         </div>
-        <div className="password">
-          <label id="password-header">Password</label>
-          <br />
-          <input type="text" id="password-input"/>  
-        </div>
-        <div>
-          <a href="" onClick={handleClick}>Sign up if you haven't already</a>
-        </div>
-
-      </form>
+      </div>
+    </div>
     </>
   );
 }
