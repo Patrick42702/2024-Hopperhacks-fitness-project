70e1416f65f773f1d7ba4cf9e64a4f6387d37498
Patrick42702 <patrick42702@yahoo.com>
2024-02-03 20:17:31 -0500

    redux changes

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counter/counterSlice'
+import contentReducer from './features/counter/contentSlice'
 
 export default configureStore({
   reducer: {
